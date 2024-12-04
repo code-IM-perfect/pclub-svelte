23572600aff72bcccc68a54c9f804abfd4d41d31
Harshit Tomar <harshitt345@gmail.com>
2024-12-04 14:28:28 +0530

    Add script to fetch cordie info
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

---
+++ src/components/+CordieList.svelte
@@ -0,0 +1,19 @@
+<script>
+	import cordieObj from '$lib/contactInfo/cordie.json';
+	// const cordies = JSON.parse(cordieJson);
+	const cordies = Object.values(cordieObj);
+
+	// console.log(cordies[0].image);
+</script>
+
+<div class="cordieContainer">
+	{#each cordies as cordie}
+		<div class="cordieCard">
+			<div class="backGlow"></div>
+			<h4>{cordie.name}</h4>
+		</div>
+	{/each}
+</div>
+
+<style>
+</style>
