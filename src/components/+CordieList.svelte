22d691117d92654097c35e3778ccc2fe3fbb6114
Harshit Tomar <harshitt345@gmail.com>
2024-12-08 16:29:31 +0530

    Add style to cordie heading
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- src/components/+CordieList.svelte
+++ src/components/+CordieList.svelte
@@ -10,4 +10,12 @@
 </div>
 
 <style>
+	h2 {
+		font-size: 5vh;
+		margin: 4vh 0;
+		color: var(--primary);
+		text-decoration: underline 0.4vh var(--secondary);
+		text-underline-offset: 0.8vh;
+		line-height: 1.3;
+	}
 </style>
