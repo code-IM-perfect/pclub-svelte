03b071170e6fc9f4ed57dc1ea3b74b99c0c1e08d
Harshit Tomar <harshitt345@gmail.com>
2024-12-11 16:42:31 +0530

    Add style to secy heading
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- src/components/+SecyList.svelte
+++ src/components/+SecyList.svelte
@@ -1,1 +1,12 @@
 <h2>Our Secretaries</h2>
+
+<style>
+	h2 {
+		text-align: center;
+		font-size: 5vh;
+		margin: 8vh 0 4vh;
+		color: var(--primary);
+		text-decoration: underline 0.4vh var(--secondary);
+		text-underline-offset: 0.8vh;
+	}
+</style>
