71c49c37ec6bca0dd5727bdac93f61620b872aac
Harshit Tomar <harshitt345@gmail.com>
2024-12-13 21:38:31 +0530

    Add styles to old cordie headings
    Signed-off-by: Harshit Tomar <harshitt345@gmail.com>

--- src/components/+OldCordies.svelte
+++ src/components/+OldCordies.svelte
@@ -1,1 +1,13 @@
 <h2>Old Cordies</h2>
+
+<style>
+	h2 {
+		text-align: center;
+		font-size: 5vh;
+		margin: 8vh 0 4vh;
+		color: var(--primary);
+		text-decoration: underline 0.4vh var(--secondary);
+		text-underline-offset: 0.8vh;
+		line-height: 1.3;
+	}
+</style>
