a5461e57325e0daf1f2b331cc5389f506992c461
Harshit Tomar <harshitt345@gmail.com>
2024-12-27 00:49:34 +0530

    Add console ascii art on initial load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import Header from '../components/+Header.svelte';
 	import Footer from '../components/+Footer.svelte';
 	let { children } = $props();
+
+	console.log(
+		" ___    _      _    \n| _ \\__| |_  _| |__ \n|  _/ _| | || | '_ \\ \n|_| \\__|_|\\_,_|_.__/\n\nWelcome to the Website of\nProgramming Club IITK\n\nTWFkZSB3aXRoIOKdpO+4jiBieSBjb2RlSU1wZXJmZWN0IA=="
+	);
 </script>
 
 <Header />
