035a4e25f488cc758f1dea4f8d1af234293b5eb5
Harshit Tomar <harshitt345@gmail.com>
2025-01-07 08:51:50 +0530

    Move basic formatting to the default component

--- src/layouts/+Default.svelte
+++ src/layouts/+Default.svelte
@@ -1,3 +1,9 @@
 <script>
 	let { children } = $props();
 </script>
+
+<article class="grid justify-center">
+	<div class="dark prose xl:prose-xl dark:prose-invert max-md:max-w-[85vw]">
+		{@render children()}
+	</div>
+</article>
