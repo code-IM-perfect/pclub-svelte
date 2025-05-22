6ca63b11426eb08001ebaa22167d9d905b2b7178
Harshit Tomar <harshitt345@gmail.com>
2025-05-22 13:58:00 +0530

    Add the roadmaps page

---
+++ src/routes/roadmaps/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	const globRoadmaps = import.meta.glob('./*/+page.svx', { eager: true });
+	const roadmaps = Object.entries(globRoadmaps);
+</script>
+
+<h1>Roadmaps</h1>
+
+<div class="cardGrid">
+	{#each roadmaps as [filePath, embedded]}
+		<div class="card">
+			<div class="cardImg"></div>
+		</div>
+	{/each}
+</div>
